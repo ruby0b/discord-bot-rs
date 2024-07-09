d0c0e24d01cbe50955363dd896bf827d0f257683
ruby0b <106119328+ruby0b@users.noreply.github.com>
2024-07-10 01:51:50 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+    let
+      forAllSystems = function: nixpkgs.lib.genAttrs
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+        (system: function (import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        }));
+
+      discord-bot-rs = { cmake, lib, makeRustPlatform, openssl, pkg-config, rust-bin, ... }:
+        let
+          nightly = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+          rustPlatform = makeRustPlatform { cargo = nightly; rustc = nightly; };
+        in
+        rustPlatform.buildRustPackage rec {
+          pname = "discord-bot-rs";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false; # no tests, compiling takes long enough as is
+          nativeBuildInputs = [ cmake pkg-config ];
+          buildInputs = [ openssl ];
+          PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+          meta.mainProgram = pname;
+        };
+    in
+    {
+      packages = forAllSystems (pkgs:
+        let exe = pkgs.callPackage discord-bot-rs { }; in {
+          default = exe;
+          discord-bot-rs = exe;
+        });
+
+      overlays = {
+        default = (final: prev: {
+          discord-bot-rs = final.callPackage discord-bot-rs { };
+        });
+        rust-overlay = rust-overlay.overlays.default;
+      };
+
+      nixosModules.default = { lib, config, pkgs, ... }:
+        let
+          inherit (lib) mkOption types mkIf;
+          name = "discord-bot-rs";
+          cfg = config.services.${name};
+        in
+        {
+          options.services.${name} = {
+            enable = mkOption { type = types.bool; default = false; };
+            autostart = mkOption { type = types.bool; default = true; };
+            token = mkOption { type = types.uniq types.str; };
+          };
+          config = mkIf cfg.enable {
+            users.users.${name} = {
+              description = name;
+              home = "/var/lib/${name}";
+              createHome = true;
+              isNormalUser = true;
+              group = name;
+            };
+            users.groups.${name} = { };
+            systemd.services.${name} = {
+              description = name;
+              wantedBy = if cfg.autostart then [ "multi-user.target" ] else [ ];
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              serviceConfig = {
+                ExecStart = "${pkgs.lib.getExe pkgs.${name}}";
+                WorkingDirectory = "/var/lib/${name}";
+                User = name;
+                Restart = "always";
+                RestartSec = 5;
+              };
+            };
+          };
+        };
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::{sync::Arc, time::Duration};
+
+use crate::prelude::*;
+use poise::serenity_prelude as serenity;
+
+pub mod consts;
+pub mod prelude;
+pub mod types;
+pub mod commands {
+    pub mod help;
+    pub mod misc;
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::help::help(), commands::misc::age()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some(consts::COMMAND_PREFIX.to_string()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            ..Default::default()
+        },
+        on_error: |error| Box::pin(on_error(error)),
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("@{} used .{}", ctx.author(), ctx.command().qualified_name);
+            })
+        },
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as {}", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .options(options)
+        .build();
+
+    let token = std::env::var("DISCORD_TOKEN").expect("Missing env var: DISCORD_TOKEN");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
+}
