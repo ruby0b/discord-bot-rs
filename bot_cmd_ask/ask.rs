bf8912544883fca243f29f161618932b7c1b536c
ruby0b <106119328+ruby0b@users.noreply.github.com>
2026-08-15 23:48:00 +0200

    🧹

--- bot_cmd_ask/ask.rs
+++ bot_cmd_ask/ask.rs
@@ -171,7 +171,7 @@ impl Ask {
 }
 
 fn user_mentions(user_ids: impl IntoIterator<Item = UserId>) -> String {
-    user_ids.into_iter().map(|u| u.mention()).join(", ")
+    user_ids.into_iter().map(|u| u.mention()).join(" ")
 }
 
 fn user_mentions_with_times(users: impl IntoIterator<Item = (DateTime<Utc>, UserId)>) -> String {
@@ -182,5 +182,5 @@ fn user_mentions_with_times(users: impl IntoIterator<Item = (DateTime<Utc>, User
             let optional_timestamp = if t > now { discord_timestamp(t) } else { "".to_string() };
             format!("{} {}", u.mention(), optional_timestamp)
         })
-        .join(", ")
+        .join(" ")
 }
